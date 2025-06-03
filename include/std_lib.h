b7dc202056ced2dfd3d44bbbad1c3d9c6737742b
knownasrayy <105399909+knownasrayy@users.noreply.github.com>
2025-06-03 23:50:25 +0700

    Update std_lib.h

--- include/std_lib.h
+++ include/std_lib.h
@@ -6,9 +6,9 @@
 int div(int a, int b);
 int mod(int a, int b);
 
-bool strcmp(char *str1, char *str2);
+bool strcmp(char *str1, char *str2); 
 void strcpy(char *dst, char *src);
-void clear(byte *buf, unsigned int size);
+void clear(byte *buf, unsigned int size); 
 
 void atoi(char *str, int *num);
 void itoa(int num, char *str);
