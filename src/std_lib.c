ef57a78c13721c5d41827ca575c8a3195e919ded
knownasrayy <105399909+knownasrayy@users.noreply.github.com>
2025-06-10 02:52:11 +0700

    Update std_lib.c

--- src/std_lib.c
+++ src/std_lib.c
@@ -1,21 +1,8 @@
-#include "std_lib.h" 
-
-void reverse(char *str, int length) {
-    int start = 0;
-    int end = length - 1;
-    char temp;
-    while (start < end) {
-        temp = str[start];
-        str[start] = str[end];
-        str[end] = temp;
-        start++;
-        end--;
-    }
-}
+#include "std_lib.h"
 
 int div(int a, int b) {
     if (b == 0) {
-        return 0;
+        return 0; 
     }
     return a / b;
 }
@@ -24,66 +11,73 @@ int mod(int a, int b) {
     if (b == 0) {
         return 0;
     }
-    return a % b;
+
+    while (a >= b) {
+        a -= b;
+    }
+    return a;
 }
 
-char strcmp(char *str1, char *str2) { 
-    while (*str1 != '\0' && *str2 != '\0') {
-        if (*str1 != *str2) {
+bool strcmp(char *str1, char *str2) {
+    int i = 0;
+    while (str1[i] != '\0' && str2[i] != '\0') {
+        if (str1[i] != str2[i]) {
             return false;
         }
-        str1++;
-        str2++;
+        i++;
     }
-    return (*str1 == *str2); 
+    return str1[i] == '\0' && str2[i] == '\0';
 }
 
 void strcpy(char *dst, char *src) {
-    while (*src != '\0') {
-        *dst = *src;
-        dst++;
-        src++;
+    int i = 0;
+    while (src[i] != '\0') {
+        dst[i] = src[i];
+        i++;
     }
-    *dst = '\0';
+    dst[i] = '\0';
 }
 
-void clear(byte *buf, unsigned int size) { 
+void clear(byte *buf, unsigned int size) {
     unsigned int i;
-    for (i = 0; i < size; i++) {
+    for (i = 0; i < size; ++i) {
         buf[i] = 0;
     }
 }
 
 void atoi(char *str, int *num) {
-    *num = 0;
-    int sign = 1;
     int i = 0;
+    bool is_negative = false;
+
+    *num = 0;
 
     if (str[0] == '-') {
-        sign = -1;
-        i++;
-    } else if (str[0] == '+') {
-        i++;
+        is_negative = true;
+        i = 1;
     }
+
 
     while (str[i] != '\0') {
         if (str[i] >= '0' && str[i] <= '9') {
-            *num = (*num * 10) + (str[i] - '0');
-        } else {
-            break;
+            *num = *num * 10 + (str[i] - '0');
         }
         i++;
     }
-    *num *= sign;
+
+    if (is_negative) {
+        *num = -(*num);
+    }
 }
 
 void itoa(int num, char *str) {
+    char temp[12];
     int i = 0;
-    char is_negative = false; 
+    int j = 0;
+    bool is_negative = false;
 
     if (num == 0) {
-        str[i++] = '0';
-        str[i] = '\0';
+        str[0] = '0';
+        str[1] = '\0';
         return;
     }
 
@@ -91,17 +85,22 @@ void itoa(int num, char *str) {
         is_negative = true;
         num = -num;
     }
-
+)
     while (num != 0) {
-        int rem = num % 10;
-        str[i++] = rem + '0';
-        num = num / 10;
+        temp[i] = mod(num, 10) + '0';
+        num = div(num, 10);
+        i++;
     }
 
     if (is_negative) {
-        str[i++] = '-';
+        str[j] = '-';
+        j++;
     }
 
-    str[i] = '\0';
-    reverse(str, i);
+    while (i > 0) {
+        i--;
+        str[j] = temp[i];
+        j++;
+    }
+    str[j] = '\0';
 }
